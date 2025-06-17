e91624c7dfd691b26deb2eba8bf5e9afef14d8a4
Shivam Sharma <shivamsharma242424@gmail.com>
2025-06-17 14:33:58 +0530

    UI improved

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:callmate/screens/home_screen/userlist_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'friend_request_screen.dart';
@@ -20,43 +21,64 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('CallMate')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const UserListScreen()),
-              );
-            },
-            child: const Text('View All Users'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal[50],
+          title: Row(
+            children: [
+              Text(
+                'CallMate',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.teal
+                ),
+              ),
+              SizedBox(width: 8),
+              Image.asset(
+                'assets/images/CallMate-Bg.png',
+                width: 36,
+                height: 36,
+              ),
+            ],
           ),
-
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const FriendRequestsScreen()),
-              );
-            },
-            child: const Text('Friend Requests'),
+          bottom: TabBar(
+            indicatorColor: Colors.teal,
+            labelColor: Colors.teal,
+            tabs: [
+              Tab(text: "All Users"),                       // Chats Tab
+              Tab(text: "Friends"),                      // Status Tab
+              Tab(text: "Friend Requests"),                       // Calls Tab
+            ],
           ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const FriendsScreen()),
-              );
-            },
-            child: const Text("View Friends"),
-          ),
-
-
-        ],
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10.0),
+              child: InkWell(
+                child: Row(
+                  children: [
+                    Text('Signout',style: TextStyle(color: Colors.teal ,fontWeight: FontWeight.w600),),
+                    SizedBox(width: 2,),
+                    Icon(Icons.logout , color: Colors.teal,),
+                  ],
+                ),
+                onTap: ()async{
+                   await FirebaseAuth.instance.signOut();
+                   Navigator.pushReplacementNamed(context, '/login');
+                },
+              ),
+            )
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            UserListScreen(),
+            FriendsScreen(),
+            FriendRequestsScreen(),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/home_screen/userlist_screen.dart
+++ lib/screens/home_screen/userlist_screen.dart
@@ -9,13 +9,13 @@ class UserListScreen extends StatefulWidget {
 }
 
 class _UserListScreenState extends State<UserListScreen> {
-
   UserRetrieval userRetrieval = UserRetrieval();
+  String buttonLabel = "Add Friend";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("All Users")),
+      backgroundColor: Colors.white,
       body: FutureBuilder<List<Map<String, dynamic>>>(
         future: userRetrieval.fetchFilteredUsers(),
         builder: (context, snapshot) {
@@ -25,25 +25,83 @@ class _UserListScreenState extends State<UserListScreen> {
 
           final users = snapshot.data ?? [];
 
-          return ListView.builder(
-            itemCount: users.length,
-            itemBuilder: (context, index) {
-              final user = users[index];
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage(user['photoUrl'] ?? ""),
-                ),
-                title: Text(user['name'] ?? "No Name"),
-                subtitle: Text(user['email']),
-                trailing: ElevatedButton(
-                  onPressed: () => userRetrieval.sendFriendRequest(user['uid']),
-                  child: const Text("Add Friend"),
-                ),
-              );
-            },
+          return users.isEmpty?Center(
+            child: Text('No User Available'),
+          ):Padding(
+            padding: const EdgeInsets.only(top: 40.0, left: 10, right: 10),
+            child: GridView.builder(
+              itemCount: users.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // Number of columns
+                crossAxisSpacing: 8, // Horizontal spacing between grid items
+                mainAxisSpacing: 8, // Vertical spacing between grid items
+                childAspectRatio: 0.75, // Width to height ratio of each item
+              ),
+              itemBuilder: (context, index) {
+                final user = users[index];
+                return Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.teal, // Border color
+                      width: 1.5, // Border width
+                    ),
+                    color: Colors.teal[50],
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 10,),
+                      Container(
+                        padding: EdgeInsets.all(2), // Border width
+                        decoration: BoxDecoration(
+                          color: Colors.transparent, // Background color
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: Colors.teal, // Border color
+                            width: 3.0,         // Border width
+                          ),
+                        ),
+                        child: CircleAvatar(
+                          radius: 40,
+                          backgroundImage: NetworkImage(user['photoUrl'] ?? ""),
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                      Text(user['name'] ?? "No Name" , style: TextStyle(fontWeight: FontWeight.w600, color: Colors.teal),),
+                      SizedBox(height: 10,),
+                      ElevatedButton(
+                        onPressed: (){
+                          userRetrieval.sendFriendRequest(user['uid']);
+                          setState(() {
+                            buttonLabel = "Request Sent";
+                          });
+                          ScaffoldMessenger.of(
+                            context,
+                          ).showSnackBar(SnackBar(content: Text("Friend Request Sent")));
+                        },
+                        child: Text(buttonLabel,style: TextStyle(color: Colors.teal),),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
           );
         },
       ),
     );
   }
 }
+
+// ListTile(
+// leading: CircleAvatar(
+// backgroundImage: NetworkImage(user['photoUrl'] ?? ""),
+// ),
+// title: Text(user['name'] ?? "No Name"),
+// subtitle: Text(user['email']),
+// trailing: ElevatedButton(
+// onPressed: () => userRetrieval.sendFriendRequest(user['uid']),
+// child: const Text("Add Friend"),
+// ),
+// );
